7f7267f2931cdcc365be905ecc8a20eeda3f0c96
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-27 09:44:38 -0700

    feat: Add methods for visitor class

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,25 @@
 class Visitor
-  
+
+  attr_reader :name, 
+              :height, 
+              :spending_money,
+              :preferences
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = spending_money.gsub("$", "").to_i
+    @preferences = []
+  end
+
+  def add_preference(preference)
+    @preferences << preference
+  end
+
+  def tall_enough?(height)
+    if @height < height
+      false
+    else 
+      true
+    end
+  end
 end
