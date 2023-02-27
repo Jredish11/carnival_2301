238092472d447ca3f6108302f79c39b8b610ef6d
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-27 09:44:17 -0700

    test: Add tests for visitor spec, iteration 1

---
+++ spec/visitor_spec.rb
@@ -0,0 +1,40 @@
+require './lib/visitor'
+
+RSpec.describe Visitor do
+  before(:each) do
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+  end
+
+
+  it 'exists' do
+    expect(@visitor1).to be_a(Visitor)
+  end
+
+  it 'has attributes' do
+    expect(@visitor1.name).to eq('Bruce')
+    expect(@visitor1.height).to eq(54)
+    expect(@visitor1.spending_money).to eq(10) 
+    expect(@visitor1.preferences).to eq([])
+  end
+
+  it 'adds_preference to preferences list' do
+    @visitor1.add_preference(:gentle)
+    @visitor1.add_preference(:water)
+    expect(@visitor1.preferences).to eq([:gentle, :water])
+  end
+
+  it 'checks height' do
+    expect(@visitor1.tall_enough?(54)).to eq(true)
+    expect(@visitor2.tall_enough?(54)).to eq(false)
+    expect(@visitor3.tall_enough?(54)).to eq(true)
+    expect(@visitor1.tall_enough?(64)).to eq(false)
+  end
+end
+
+
+
+
+
+
